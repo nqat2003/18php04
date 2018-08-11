56a86c60bc9bc11f75c40601bb333db74d64011c
Anh Tai <nqat2003@gmail.com>
2018-08-11 11:39:26 +0700

    update session 11

--- ex_session11/formTinhLuong/common.js
+++ ex_session11/formTinhLuong/common.js
@@ -1,7 +1,7 @@
 function doSalary(){
 	var basicS= parseInt(document.getElementById('coban').value);
-	var coeS=parseInt(document.getElementById('heso').value);
-	var allowaS=parseInt(document.getElementById('phucap').value);
+	var coeS= parseInt(document.getElementById('heso').value);
+	var allowaS= parseInt(document.getElementById('phucap').value);
 	var salary = getSalary(basicS,coeS,allowaS);
 	printSalary(salary);
 }
@@ -9,13 +9,15 @@ function getSalary(basicS,coeS,allowaS) {
 	if (coeS>5) return basicS*coeS;
 	else return basicS*coeS+allowaS;
 }
-function printSalary(luong){
-	document.getElementById('result').innerHTML="Lương của bạn là: " + luong;
-	if (luong>30000000){
+function printSalary(salary){
+	document.getElementById('result').innerHTML="Lương của bạn là: " + salary;
+	if (salary>30000000){
 		document.getElementById('cmt').innerHTML="Bạn quá giàu";
 		document.getElementById('cmt').style.color='blue';	
-	}else if(luong>5000000){
+	}else if(salary<5000000){
 		document.getElementById('cmt').innerHTML="Bạn cần cố gắng hơn";
 		document.getElementById('cmt').style.color='red';
-	} 
+		}else{
+			document.getElementById('cmt').innerHTML="";
+		} 
 }
